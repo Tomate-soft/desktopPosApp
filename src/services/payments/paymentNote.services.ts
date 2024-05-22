e62b31f18ba2fa7fc07143d3ed4130b5f92e6c73
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2024-05-22 17:20:38 -0600

    chore: :rocket: Healtcheck payment notes
    Healtcheck payment notes

--- src/services/payments/paymentNote.services.ts
+++ src/services/payments/paymentNote.services.ts
@@ -1,9 +1,8 @@
 import axios from "../../configs/axios";
-
-const URL_PATH = "http://localhost:8000/payments/p/note/";
+import { PAYMENT_NOTES_PATH } from "../../lib/routes.paths.lib";
 
 export const paymentNoteService = async (id: string, body: any) => {
-  const response = axios.post(`${URL_PATH}${id}`, body);
+  const response = axios.post(`${PAYMENT_NOTES_PATH}/${id}`, body);
   return response;
 };
 // REVISAR SI ACA NOS QUEDAMOS - AL PARECER TRABAJABAMOS EN LOCAL UNA VES TEMRINADO MIGRAR AL NUEVO BACKEND
